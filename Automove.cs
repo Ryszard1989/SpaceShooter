0ce22d75be2923ac30dbaa8f2d45aa8b150671d2
Ryszard Szulc <rys.szulc@gmail.com>
2017-01-22 20:17:58 +0000

    Working Copy. Can revert Shotspawn in Shotspawns easily enough

---
+++ Automove.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Automove : MonoBehaviour {
+
+    public float dodge;
+    public float smoothing;
+    public float tilt;
+    public Vector2 startWait;    
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public Boundary boundary;
+
+    private float targetManeuver;
+    private float currentSpeed;
+    private Rigidbody rb;
+
+
+    public GameObject shot;
+    public Transform[] shotSpawns;
+    public float fireRate;
+
+    private float nextFire;
+
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
+	
+	}
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+        while (true)
+        {
+            targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            foreach (var shotSpawn in shotSpawns)
+            {
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            }
+        }
+    }
+
+
+    void FixedUpdate ()
+    {
+        float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+        rb.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
---
+++ DestroyByContact.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start ()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameControllerObject == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if(other.tag == "Boundary" || other.tag == "Enemy")
+        if(other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        Debug.Log("DestroyByContact:Other: " + other);
+        //*COMMENT TO TEST*
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+
+    //Handle Asteroids at spawn if don't want bounce
+    //void OnTriggerStay(Collider other)
+    //{
+    //    Instantiate(explosion, transform.position, transform.rotation);
+    //    Destroy(other.gameObject);
+    //    Destroy(gameObject);
+    //}
+
+}
